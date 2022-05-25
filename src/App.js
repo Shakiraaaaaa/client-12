db8f956c30636ed155e86a691c689812b45e46cb
unknown <tabassumshakira999@gmail.com>
2022-05-26 03:40:12 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import MyAppointments from './pages/Dashboard/MyAppointments';
 import MyReview from './pages/Dashboard/MyReview';
 import MyHistory from './pages/Dashboard/MyHistory';
+import Users from './pages/Dashboard/Users';
 
 function App() {
   return (
@@ -32,7 +33,9 @@ function App() {
           <Route index element={<MyAppointments></MyAppointments>}></Route>
           <Route path="review" element={<MyReview></MyReview>}></Route>
           <Route path="history" element={<MyHistory></MyHistory>}></Route>
+          <Route path="users" element={<Users></Users>}></Route>
         </Route>
+        <Route path="users" element={<Users></Users>}></Route>
         <Route path="signup" element={<SignUp></SignUp>} />
       </Routes>
       <ToastContainer></ToastContainer>
--- src/pages/Dashboard/MyAppointments.js
+++ src/pages/Dashboard/MyAppointments.js
@@ -1,3 +1,5 @@
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
@@ -6,14 +8,34 @@ const MyAppointments = () => {
 
     const [appointments, setAppointments] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate()
+
 
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/booking?patient=${user.email}`)
-                .then(res => res.json())
-                .then(data => setAppointments(data));
+            fetch(`http://localhost:5000/booking?patient=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => {
+                    console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/');
+                    }
+                    return res.json()
+                })
+                .then(data => {
+
+                    setAppointments(data);
+                });
         }
     }, [user])
+
+
 
     return (
         <div>
@@ -31,7 +53,7 @@ const MyAppointments = () => {
                     </thead>
                     <tbody>
                         {
-                            appointments.map((a, index) =><tr>
+                            appointments.map((a, index) => <tr>
                                 <th>{index + 1}</th>
                                 <td>{a.patientName}</td>
                                 <td>{a.date}</td>
@@ -39,8 +61,8 @@ const MyAppointments = () => {
                                 <td>{a.treatment}</td>
                             </tr>)
                         }
-                        
-                        
+
+
                     </tbody>
                 </table>
             </div>
